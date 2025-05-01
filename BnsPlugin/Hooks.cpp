3d45ebced1b7f1b1e5e1d4cb78d26603f9560972
Leon W. <31262614+leanleon93@users.noreply.github.com>
2025-05-01 05:03:28 +0200

    add skillswap

--- BnsPlugin/Hooks.cpp
+++ BnsPlugin/Hooks.cpp
@@ -184,6 +184,119 @@ void RemoveAnimationsForRecord(BnsTables::KR::skillshow3_Record* record)
 	}
 }
 
+#ifdef _BNSEU
+void SwapAnimationsForRecords(BnsTables::EU::skillshow3_Record* record1, BnsTables::EU::skillshow3_Record* record2)
+#elif _BNSKR
+void SwapAnimationsForRecords(BnsTables::KR::skillshow3_Record* record1, BnsTables::KR::skillshow3_Record* record2)
+#endif
+{
+	for (auto [member1, member2] : {
+		//add all from above
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_cast_show, record2->player_cast_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_exec_show_1, record2->player_exec_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_exec_show_2, record2->player_exec_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_exec_show_3, record2->player_exec_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_exec_show_4, record2->player_exec_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_exec_show_5, record2->player_exec_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_fire_show_1, record2->player_fire_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_fire_show_2, record2->player_fire_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_fire_show_3, record2->player_fire_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_fire_show_4, record2->player_fire_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_fire_show_5, record2->player_fire_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->player_stop_show, record2->player_stop_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->cast_show, record2->cast_show),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->decal_show_1, record2->decal_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_show_1, record2->fire_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_miss_show_1, record2->fire_miss_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_show_1, record2->exec_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_hit_show_1, record2->exec_hit_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_immune_show_1, record2->exec_immune_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_dodge_show_1, record2->exec_dodge_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_parry_show_1, record2->exec_parry_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_show_1, record2->exec_bounce_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_caster_show_1, record2->exec_bounce_caster_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_counter_show_1, record2->exec_counter_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_critical_show_1, record2->exec_critical_show_1),
+		std::pair<wchar_t*&, wchar_t*&>(record1->return_show_1, record2->return_show_1),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->decal_show_2, record2->decal_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_show_2, record2->fire_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_miss_show_2, record2->fire_miss_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_show_2, record2->exec_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_hit_show_2, record2->exec_hit_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_immune_show_2, record2->exec_immune_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_dodge_show_2, record2->exec_dodge_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_parry_show_2, record2->exec_parry_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_show_2, record2->exec_bounce_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_caster_show_2, record2->exec_bounce_caster_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_counter_show_2, record2->exec_counter_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_critical_show_2, record2->exec_critical_show_2),
+		std::pair<wchar_t*&, wchar_t*&>(record1->return_show_2, record2->return_show_2),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->decal_show_3, record2->decal_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_show_3, record2->fire_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_miss_show_3, record2->fire_miss_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_show_3, record2->exec_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_hit_show_3, record2->exec_hit_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_immune_show_3, record2->exec_immune_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_dodge_show_3, record2->exec_dodge_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_parry_show_3, record2->exec_parry_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_show_3, record2->exec_bounce_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_caster_show_3, record2->exec_bounce_caster_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_counter_show_3, record2->exec_counter_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_critical_show_3, record2->exec_critical_show_3),
+		std::pair<wchar_t*&, wchar_t*&>(record1->return_show_3, record2->return_show_3),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->decal_show_4, record2->decal_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_show_4, record2->fire_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_miss_show_4, record2->fire_miss_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_show_4, record2->exec_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_hit_show_4, record2->exec_hit_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_immune_show_4, record2->exec_immune_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_dodge_show_4, record2->exec_dodge_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_parry_show_4, record2->exec_parry_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_show_4, record2->exec_bounce_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_caster_show_4, record2->exec_bounce_caster_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_counter_show_4, record2->exec_counter_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_critical_show_4, record2->exec_critical_show_4),
+		std::pair<wchar_t*&, wchar_t*&>(record1->return_show_4, record2->return_show_4),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->decal_show_5, record2->decal_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_show_5, record2->fire_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->fire_miss_show_5, record2->fire_miss_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_show_5, record2->exec_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_hit_show_5, record2->exec_hit_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_immune_show_5, record2->exec_immune_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_dodge_show_5, record2->exec_dodge_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_parry_show_5, record2->exec_parry_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_show_5, record2->exec_bounce_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_bounce_caster_show_5, record2->exec_bounce_caster_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_counter_show_5, record2->exec_counter_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->exec_critical_show_5, record2->exec_critical_show_5),
+		std::pair<wchar_t*&, wchar_t*&>(record1->return_show_5, record2->return_show_5),
+
+		std::pair<wchar_t*&, wchar_t*&>(record1->finish_show, record2->finish_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->bomb_show, record2->bomb_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->cancel_show, record2->cancel_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->stop_show, record2->stop_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->triggered_show, record2->triggered_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_cast_show, record2->trigger_cast_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->triggered_enemy_show, record2->triggered_enemy_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_show, record2->trigger_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_hit_show, record2->trigger_hit_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_immune_show, record2->trigger_immune_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_dodge_show, record2->trigger_dodge_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_parry_show, record2->trigger_parry_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_bounce_show, record2->trigger_bounce_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_counter_show, record2->trigger_counter_show),
+		std::pair<wchar_t*&, wchar_t*&>(record1->trigger_critical_show, record2->trigger_critical_show)
+		}) {
+		member1 = member2;
+	}
+}
+
+
 BnsTables::Shared::DrEl* (__fastcall* oFind_b8)(DrMultiKeyTable* thisptr, unsigned __int64 key);
 BnsTables::Shared::DrEl* __fastcall hkFind_b8(DrMultiKeyTable* thisptr, unsigned __int64 key) {
 	if (!g_PluginConfig.AnimFilterEnabled() || !g_PluginConfig.IsLoaded() || !g_PluginConfig.HasActiveProfile()) {
@@ -192,6 +305,31 @@ BnsTables::Shared::DrEl* __fastcall hkFind_b8(DrMultiKeyTable* thisptr, unsigned
 	if (thisptr->_tabledef->type != g_SkillIdManager.GetSkillshowTableId()) return oFind_b8(thisptr, key);
 	const auto& ids = g_SkillIdManager.GetIdsToFilter();
 	const auto skillId = static_cast<int32_t>(key);
+
+#ifdef _SKILLSWAP2
+	const auto& profile = g_PluginConfig.GetActiveProfile();
+	if (const auto& swap = profile.AnimationSwaps.find(skillId); swap != profile.AnimationSwaps.end()) {
+		auto animationSwapKey = SkillIdManager::SkillShow3KeyHelper::BuildKey(swap->second.AnimationSkillId, (short)swap->second.AnimationSkillVariation, 0);
+		auto animSwapRecordBase = oFind_b8(thisptr, animationSwapKey);
+		if (animSwapRecordBase == nullptr) return oFind_b8(thisptr, key);
+#ifdef _BNSEU
+		auto animSwapRecord = (BnsTables::EU::skillshow3_Record*)animSwapRecordBase;
+#elif _BNSKR
+		auto animSwapRecord = (BnsTables::KR::skillshow3_Record*)animSwapRecordBase;
+#endif
+		auto recordBase = oFind_b8(thisptr, key);
+		if (recordBase == nullptr) return oFind_b8(thisptr, key);
+#ifdef _BNSEU
+		auto record = (BnsTables::EU::skillshow3_Record*)recordBase;
+#elif _BNSKR
+		auto record = (BnsTables::KR::skillshow3_Record*)recordBase;
+#endif
+		SwapAnimationsForRecords(record, animSwapRecord);
+		reloadRequired = true;
+		return record;
+	}
+#endif
+
 #ifdef _DEBUG
 	//print the skill id and variation id from record_d
 	auto skillshowKey_d = SkillIdManager::SkillShow3KeyHelper::ExtractKey(key);
